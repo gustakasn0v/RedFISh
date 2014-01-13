6698414ee19a3aabd00424839e92965ee797bb7f
dreabalbas <drea.balbas@gmail.com>
2014-01-13 08:05:09 -0430

    Javadoc

--- AuthDatabase.java
+++ AuthDatabase.java
@@ -1,14 +1,37 @@
 import java.rmi.*;
 import java.util.LinkedList;
 
-/** The RMI client will use this interface directly.
- *  The RMI server will make a real remote object that
- *  implements this, then register an instance of it
- *  with some URL.
+/**
+ * AuthDatabase.java
+ *
+ * Septiembre - Diciembre 2013
+ *
+ * Interfaz de la clase que se utilizará para la autenticación de los usuarios.
+ * Extiende a la interfaz Remote.
+ *
+ * @author Andrea Balbás        09-10076
+ * @author Gustavo El Khoury    10-10226
  */
-
 public interface AuthDatabase extends Remote {
+
+        /**
+        * Método para agregar las credenciales de los usuarios que tienen 
+        * acceso al servidor de archivos.
+        * 
+        * @param user Usuario cuyas credenciales serán agregadas.
+        * @return true en caso de que el usuario sea correctamente agregado.
+        *         false en caso contrario.
+        * @throws RemoteException En caso de error en la llamada remota. 
+        */
 	public Boolean addUser(User user) throws RemoteException;
 
+	/**
+        * Valida que un usuario pertenezca a los usuarios con acceso al 
+        * servidor de archivos.
+        * 
+        * @param user Usuario cuyas credenciales serán validadas.
+        * @return Lista con los usuarios cuyas credenciales son correctas.
+        * @throws RemoteException En caso de error en la llamada remota. 
+        */
 	public LinkedList<User> authenticate(LinkedList<User> credentials) throws RemoteException;
 }
--- AuthDatabaseImpl.java
+++ AuthDatabaseImpl.java
@@ -6,21 +6,44 @@ import java.io.InputStreamReader;
 import java.util.Hashtable;
 import java.util.LinkedList;
 
-/** This is the actual implementation of Rem that
- *  the RMI server uses. The server builds an instance
- *  of this then registers it with a URL. The
- *  client accesses the URL and binds the result to
- *  a Rem (not a RemImpl; it doesn't have this).
+/**
+ * AuthDatabaseImpl.java
+ *
+ * Septiembre - Diciembre 2013
+ *
+ * Implementación de la interfaz AuthDatabase, que se utilizará para la
+ * autenticación de usuarios.
+ * Extiende a la clase UnicastRemoteObject e implementa AuthDatabase.
+ *
+ * @author Andrea Balbás        09-10076
+ * @author Gustavo El Khoury    10-10226
  */
-
 public class AuthDatabaseImpl extends UnicastRemoteObject implements AuthDatabase {
+  
+  /**
+  * Tabla de hash en la que se almacenan las credenciales de los usuarios.
+  * La clave corresponde al nombre de usuario, y el valor es la clave de acceso
+  * asociada a ese usuario.
+  */
   private Hashtable<String,String> credentialDB;
 
+  /**
+   * Constructor de la clase.
+   * @throws RemoteException Si no se puede contactar el registro de autenticación.
+   */
   public AuthDatabaseImpl() throws RemoteException {
     this.credentialDB = new Hashtable<String,String>();
   }
 
-
+  /**
+   * Método para agregar las credenciales de los usuarios que tienen 
+   * acceso al servidor de archivos.
+   * 
+   * @param user Usuario cuyas credenciales serán agregadas.
+   * @return true en caso de que el usuario sea correctamente agregado.
+   *         false en caso contrario.
+   * @throws RemoteException En caso de error en la llamada remota. 
+   */
   public Boolean addUser(User user) throws RemoteException{
     if ( this.credentialDB.get(user.username) != null) return false;
     else{
@@ -29,6 +52,14 @@ public class AuthDatabaseImpl extends UnicastRemoteObject implements AuthDatabas
     }
   }
 
+  /**
+   * Valida que un usuario pertenezca a los usuarios con acceso al 
+   * servidor de archivos.
+   * 
+   * @param user Usuario cuyas credenciales serán validadas.
+   * @return Lista con los usuarios cuyas credenciales son correctas.
+   * @throws RemoteException En caso de error en la llamada remota. 
+   */
   public LinkedList<User> authenticate(LinkedList<User> credentials) throws RemoteException{
     LinkedList<User> authenticated = new LinkedList<User>();
     System.out.println(""+credentials.size());
